27ffc4133f86625589615391f047d272c6c5302b
jahedahmed <jahedahmed@localhost>
2020-05-26 05:51:08 +0200

    first commit

---
+++ hi/src/hi/jahed.java
@@ -0,0 +1,17 @@
+package hi;
+import java.time.*; 
+import java.util.Scanner; // import the Scanner class 
+
+
+
+import java.time.LocalDate; 
+
+public class jahed {  
+	  public static void main(String[] args) { 
+		    Integer myInt = 10000687; 
+		    String myString = myInt.toString();
+		    System.out.println(myString.length());
+		  }
+	  }
+
+
